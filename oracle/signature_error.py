ab46c30c71fdf82b62a3694cb755ba51d733090d
Abdulganiev <300195@mail.ru>
2022-04-16 12:59:10 +0500

    oracle add report GKV and signature error

---
+++ oracle/signature_error.py
@@ -0,0 +1,56 @@
+import jaydebeapi
+import json
+
+path = "access to the database.txt"
+with open(path) as f:
+    access = json.load(f)
+    
+driver = 'ojdbc14.jar'
+path_base = access['path_base']
+password = access['password']
+login = access['login']
+port = access['port']
+sid = access['sid']
+
+conn = jaydebeapi.connect(
+    'oracle.jdbc.driver.OracleDriver',
+    f'jdbc:oracle:thin:{login}/{password}@{path_base}:{port}/{sid}',
+    [login, password],
+    driver)
+
+curs = conn.cursor()
+
+def alarm_ep():
+    curs.execute(
+    '''select count(*) from
+     (select id
+       from uszn.all_smev3_inc_messages
+       where date_created>=To_Date('01.01.2021') and proc_status_id in (3))'''
+    )
+    cnt = int(curs.fetchall()[0][0])
+
+    if cnt > 0:
+        curs.execute(
+        '''
+        declare
+          iiIDs        uszn.pkGen.TIntegers;
+          i$ Pls_Integer;
+        begin
+        select id bulk collect into iiIDs
+          from uszn.all_smev3_inc_messages
+          where date_created>=To_Date('01.01.2021') and proc_status_id in (3);
+          for i in 1..iiIDs.count loop
+            begin
+              i$ := uszn.pkSMEV3.ProcessInMessage(iiIDs(i), 1, 1,'Принудительная обработка входящего сообщения СМЭВ-3');
+            commit;
+            exception
+              when Others then
+                rollback;
+                raise;
+            end;
+          end loop;
+        end;
+        '''
+        )
+
+alarm_ep()
