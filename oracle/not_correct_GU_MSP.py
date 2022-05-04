3dff0a8e420347a966ef6f0595913db37ee44e2b
Abdulganiev <300195@mail.ru>
2022-05-04 19:54:18 +0500

    add not correct GU

---
+++ oracle/not_correct_GU_MSP.py
@@ -0,0 +1,204 @@
+import jaydebeapi
+import json
+import pandas as pd
+from writing_to_log_file import *
+from smtp import *
+
+
+path = "access_report.txt"
+with open(path) as f:
+    access = json.load(f)
+    
+driver = 'ojdbc14.jar'
+path_base = access['path_base']
+password = access['password']
+login = access['login']
+port = access['port']
+sid = access['sid']
+
+conn = jaydebeapi.connect(
+    'oracle.jdbc.driver.OracleDriver',
+    f'jdbc:oracle:thin:{login}/{password}@{path_base}:{port}/{sid}',
+    [login, password],
+    driver)
+
+curs = conn.cursor()
+
+#***************************************************************
+def not_correct_GU_MSP():
+    curs.execute('''
+select t.pc_id, t.region_id, t.id, t.message_guid, t.adr, t.request_pdoc_id, t.smev_message_id, t.state_service_name,
+ (case when upper(t.adr) like '%НОВЫЙ%' then 58
+       when upper(t.adr) like '%КРАСНОСЕЛЬКУП%' then 59
+       when upper(t.adr) like '%САЛЕХАРД%' then 60
+       when upper(t.adr) like '%ЛАБЫТНАНГ%' then 62
+       when upper(t.adr) like '%ХАРП%' then 62
+       when upper(t.adr) like '%ПРИУРАЛ%' then 61
+       when upper(t.adr) like '%НАДЫМ%' then 63
+       when upper(t.adr) like '%ГУБК%' then 64
+       when upper(t.adr) like '%ПУРПЕ%' then 64
+       when upper(t.adr) like '%МУРАВ%' then 65
+       when upper(t.adr) like '%НОЯБ%' then 66
+       when upper(t.adr) like '%ПУРОВСК%' then 67
+       when upper(t.adr) like '%ЯМАЛЬ%' then 68
+       when upper(t.adr) like '%ШУРЫШ%' then 69
+       when upper(t.adr) like '%ТАЗ%' then 70
+   else 0 end) as mo_out
+from
+(select
+  r.region_id||'-'||r.pc_id as pc_id,
+  r.region_id,
+  r.id,
+  uszn.pkXMLUtils.GUIDToStr(r.message_guid) as message_guid,
+  
+  (select
+ NVL(uszn.pkPerson.GetPersonalReq(region_id, id, 15),
+    (select value from uszn.all_personal_doc_reqs
+      where region_id=pc.region_id and pdoc_id=uszn.pkPerson.GetMainPersonIdentity(pc.region_id, pc.id, 0) and class_id=8434))
+ from uszn.v_people_and_colls pc
+ where pc.region_id=r.region_id and pc.id=r.pc_id) as adr,
+ 
+ r.request_pdoc_id,
+ r.smev_message_id,
+ r.state_service_name
+
+from
+  uszn.all_ssvc_requests r
+where
+  r.region_id=71 and r.date_created>=To_Date('01.01.2022') and
+  (r.state_svc_region_id,r.state_svc_id) in ((104, 3),(104, 5),(104, 6),(104, 7),(104, 8),(104,10),(104,11),(104,25),(104,16),
+                                             (104,12),(104,20),(104,21),(104,24),(104,26),(104,28),
+                                             (104,29),(104,34),(104,35),(104,37),(104,38),(104,39),(104,40),
+                                             (104,13),(104,14))
+  and r.request_origin_id in (2,5,1,4) and r.status_id not in (40,50)
+  and r.smev3_inc_message_id is null
+  and r.is_test_request=0
+  and r.data_kind_id is null
+  and upper(r.case_number) not like ('%TEST%')) t''')
+    
+    data = {
+         'pc_id' : [],
+         'region_id' : [],
+         'id' : [],
+         'message_guid' : [],
+         'adr' : [],
+         'request_pdoc_id' : [],
+         'smev_message_id' : [],
+         'state_service_name' : [],
+         'mo_out' : [],
+        }
+    
+    for row in curs.fetchall():
+        data['pc_id'].append(row[0])
+        data['region_id'].append(row[1])
+        data['id'].append(row[2])
+        data['message_guid'].append(row[3])
+        data['adr'].append(row[4])
+        data['request_pdoc_id'].append(row[5])
+        data['smev_message_id'].append(row[6])
+        data['state_service_name'].append(row[7])
+        data['mo_out'].append(row[8])
+        
+    return data
+
+#***************************************************************
+
+def ararm_sql(sql, log, mail):
+    try:
+        curs.execute(sql)
+        return 1
+    except:
+        text = f'ошибка \n {sql}'
+        name_log = f'Alarm sql - {log}'
+        writing_to_log_file(name_log, text)
+        send_email(mail, name_log, msg_text=text)
+        return 0
+
+#***************************************************************    
+def not_correct_GU_MSP_transfer(df):
+    for row in df.itertuples(index=False):
+        if row[8] != 0:
+            pc_id = row[0]
+            region_id = row[1]
+            id = row[2]
+            message_guid = row[3]
+            adr = row[4]
+            request_pdoc_id = row[5]
+            smev_message_id = row[6]
+            state_service_name = row[7]
+            mo_out = row[8]
+            
+            sql = f'''begin
+    delete from uszn.r_ssvc_requests where region_id={region_id} and id={id};
+    update uszn.all_smev2_inc_messages set region_id={mo_out}
+        where message_guid=uszn.pkXMLUtils.StrToGuid('{message_guid}');
+    declare
+        rMessageGUID Raw(16) := uszn.pkXMLUtils.StrToGuid('{message_guid}');
+        bMessageData BLOB;
+        iSOAPBodyTagID Pls_Integer;
+        iOperationTagID Pls_Integer;
+    begin
+        begin
+            select request_data into bMessageData from uszn.r_smev_messages where message_guid=rMessageGUID;
+        exception
+            when No_Data_Found then Raise_Application_Error(-20000, 'Сообщение СМЭВ-2 не найдено');
+        end;
+        uszn.pkXMLImp.ParseXMLData(bMessageData, 1, 1, 1);
+        uszn.pkXMLImp.ComputeNSAttribs(1, 1);
+        uszn.pkSMEVProv.SetSvcSMEVNamespace(104, 8);
+        iSOAPBodyTagID := uszn.pkXML_SOAP.GetBodyTagID(1);
+        iOperationTagID := uszn.pkXMLImp.GetChildTagID(iSOAPBodyTagID, 1, 0);
+        uszn.pkWSStateSvc.ProcessCreateApplication(rMessageGUID, iOperationTagID, 1);
+    end;
+    declare
+        iiIDs uszn.pkGen.TIntegers;
+        i$ Pls_Integer;
+    begin
+      select id bulk collect into iiIDs
+        from uszn.all_personal_doc_files
+        where region_id={region_id} and pdoc_id={request_pdoc_id}
+      order by id;
+      for i in 1..iiIDs.count loop
+        begin
+          uszn.pkPerson.DeletePDocFile({region_id},iiIDs(i),1);
+        end;
+      end loop;
+    end;
+    delete from uszn.r_personal_docs where region_id={region_id} and id={request_pdoc_id};
+    begin
+      uszn.pkSMEVProv.ScheduleAsyncProcessing(uszn.pkSMEVProv.GetMessageGUID({smev_message_id},1));
+    end;
+end;'''
+            
+            name_log = 'not_correct_GU_MSP'
+            mail = 'IVAbdulganiev@yanao.ru'
+
+            resume = ararm_sql(sql, name_log, mail)
+
+            if resume == 1:
+                text = f'Заявитель {pc_id} c МСП {state_service_name} перенесен в {mo_out}, его адрес {adr}'
+
+                writing_to_log_file(name_log, text)
+                send_email(mail, name_log, msg_text=text)
+        else:
+            text = f'Заявитель {pc_id} c МСП {state_service_name} - проблема с адресом - {adr}'
+            name_log = '!!! not_correct_GU_MSP'
+            mail = 'IVAbdulganiev@yanao.ru'
+            
+            writing_to_log_file(name_log, text)
+            send_email(mail, name_log, msg_text=text)
+
+#***************************************************************
+
+data = not_correct_GU_MSP()
+df = pd.DataFrame(data)
+
+if len(set(data['pc_id'])) > 0:
+    not_correct_GU_MSP_transfer(df)
+else:
+    text = 'Некорретных заявлений МСП нет'
+    name_log = 'not_correct_GU_MSP'
+    mail = 'IVAbdulganiev@yanao.ru'
+
+    writing_to_log_file(name_log, text)
+    send_email(mail, name_log, msg_text=text)
---
+++ oracle/not_correct_GU_PGS.py
@@ -0,0 +1,182 @@
+import jaydebeapi
+import json
+import pandas as pd
+from writing_to_log_file import *
+from smtp import *
+
+
+path = "access_report.txt"
+with open(path) as f:
+    access = json.load(f)
+    
+driver = 'ojdbc14.jar'
+path_base = access['path_base']
+password = access['password']
+login = access['login']
+port = access['port']
+sid = access['sid']
+
+conn = jaydebeapi.connect(
+    'oracle.jdbc.driver.OracleDriver',
+    f'jdbc:oracle:thin:{login}/{password}@{path_base}:{port}/{sid}',
+    [login, password],
+    driver)
+
+curs = conn.cursor()
+
+
+#***************************************************************
+def not_correct_GU_PGS():
+    curs.execute('''
+select t.pc_id, t.region_id, t.id, t.message_guid, t.adr, t.request_pdoc_id, t.smev_message_id, t.state_service_name,
+ (case when upper(t.adr) like '%НОВЫЙ%' then 58
+       when upper(t.adr) like '%КРАСНОСЕЛЬКУП%' then 59
+       when upper(t.adr) like '%САЛЕХАРД%' then 60
+       when upper(t.adr) like '%ЛАБЫТНАНГ%' then 62
+       when upper(t.adr) like '%ХАРП%' then 62
+       when upper(t.adr) like '%ПРИУРАЛ%' then 61
+       when upper(t.adr) like '%НАДЫМ%' then 63
+       when upper(t.adr) like '%ГУБК%' then 64
+       when upper(t.adr) like '%ПУРПЕ%' then 64
+       when upper(t.adr) like '%МУРАВ%' then 65
+       when upper(t.adr) like '%НОЯБ%' then 66
+       when upper(t.adr) like '%ПУРОВСК%' then 67
+       when upper(t.adr) like '%ЯМАЛЬ%' then 68
+       when upper(t.adr) like '%ШУРЫШ%' then 69
+       when upper(t.adr) like '%ТАЗ%' then 70
+   else 0 end) as mo_out
+from
+(select
+  r.region_id||'-'||r.pc_id as pc_id,
+  r.region_id,
+  r.id,
+  uszn.pkXMLUtils.GUIDToStr(r.message_guid) as message_guid,
+  
+  (select
+ NVL(uszn.pkPerson.GetPersonalReq(region_id, id, 15),
+    (select value from uszn.all_personal_doc_reqs
+      where region_id=pc.region_id and pdoc_id=uszn.pkPerson.GetMainPersonIdentity(pc.region_id, pc.id, 0) and class_id=8434))
+ from uszn.v_people_and_colls pc
+ where pc.region_id=r.region_id and pc.id=r.pc_id) as adr,
+ 
+ r.request_pdoc_id,
+ r.smev3_inc_message_id as smev_message_id,
+ r.state_service_name
+
+from
+  uszn.all_ssvc_requests r
+  inner join
+  uszn.all_smev3_data_kinds t2
+on
+  r.region_id=71 and r.date_created>=To_Date('01.01.2022') and
+  r.data_kind_region_id=t2.region_id and r.data_kind_id=t2.id
+  and r.status_id in (1, 2, 3, 4, 10, 20, 30, 40)
+  and (r.region_id, r.id) not in
+      ((104, 1), (104, 2), (104, 18), (104, 27), (104, 30), (104, 9),
+       (104, 31), (104, 13), (104, 14), (104, 15), (104, 19), (104, 17), (104, 32))
+  and pc_id != 140264 ) t''')
+    
+    data = {
+         'pc_id' : [],
+         'region_id' : [],
+         'id' : [],
+         'message_guid' : [],
+         'adr' : [],
+         'request_pdoc_id' : [],
+         'smev_message_id' : [],
+         'state_service_name' : [],
+         'mo_out' : [],
+        }
+    
+    for row in curs.fetchall():
+        data['pc_id'].append(row[0])
+        data['region_id'].append(row[1])
+        data['id'].append(row[2])
+        data['message_guid'].append(row[3])
+        data['adr'].append(row[4])
+        data['request_pdoc_id'].append(row[5])
+        data['smev_message_id'].append(row[6])
+        data['state_service_name'].append(row[7])
+        data['mo_out'].append(row[8])
+        
+    return data
+
+#***************************************************************
+
+def ararm_sql(sql, log, mail):
+    try:
+        curs.execute(sql)
+        return 1
+    except:
+        text = f'ошибка \n {sql}'
+        name_log = f'Alarm sql - {log}'
+        writing_to_log_file(name_log, text)
+        send_email(mail, name_log, msg_text=text)
+        return 0    
+
+#***************************************************************
+def not_correct_GU_PGS_transfer(df):
+    for row in df.itertuples(index=False):
+        if row[8] != 0:
+            pc_id = row[0]
+            region_id = row[1]
+            id = row[2]
+            message_guid = row[3]
+            adr = row[4]
+            request_pdoc_id = row[5]
+            smev_message_id = row[6]
+            state_service_name = row[7]
+            mo_out = row[8]
+            sql = f'''begin -- заявитель {region_id}-{pc_id}
+    delete from uszn.r_ssvc_requests where region_id={region_id} and id={id};
+    update uszn.r_smev3_inc_messages set region_id={mo_out} -- {adr}
+        where message_guid=uszn.pkXMLUtils.StrToGuid('{message_guid}');
+    begin
+        uszn.pkSMEV3.ScheduleInMsgProcessing('{smev_message_id}');
+    end;
+    declare
+      iiIDs uszn.pkGen.TIntegers;
+      i$ Pls_Integer;
+    begin
+      select id bulk collect into iiIDs
+        from uszn.all_personal_doc_files
+        where region_id={region_id} and pdoc_id={request_pdoc_id}
+      order by id;
+      for i in 1..iiIDs.count loop
+        begin
+          uszn.pkPerson.DeletePDocFile({region_id},iiIDs(i),1);
+        end;
+      end loop;
+    end;
+    delete from uszn.r_personal_docs where region_id={region_id} and id={request_pdoc_id};
+end;'''
+            name_log = 'not_correct_GU_PGS'
+            mail = 'IVAbdulganiev@yanao.ru'
+
+            resume = ararm_sql(sql, name_log, mail)
+            if resume == 1:
+                text = f'Заявитель {pc_id} c ПГС {state_service_name} перенесен в {mo_out}, его адрес {adr}'
+                writing_to_log_file(name_log, text)
+                send_email(mail, name_log, msg_text=text)
+        else:
+            text = f'Заявитель {pc_id} c ПГС {state_service_name} - проблема с адресом - {adr}'
+            name_log = '!!! not_correct_GU_PGS'
+            mail = 'IVAbdulganiev@yanao.ru'
+            
+            writing_to_log_file(name_log, text)
+            send_email(mail, name_log, msg_text=text)
+
+#***************************************************************
+
+data = not_correct_GU_PGS()
+df = pd.DataFrame(data)
+
+if len(set(data['pc_id'])) > 0:
+    not_correct_GU_PGS_transfer(df)
+else:
+    text = 'Некорретных заявлений ПГС нет'
+    name_log = 'not_correct_GU_PGS'
+    mail = 'IVAbdulganiev@yanao.ru'
+
+    writing_to_log_file(name_log, text)
+    send_email(mail, name_log, msg_text=text)
