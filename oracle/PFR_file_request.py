0fb19a63a033c054e06d518d27a7b270c1d49723
Abdulganiev <300195@mail.ru>
2022-05-24 21:22:12 +0500

    added log

--- oracle/PFR_file_request.py
+++ oracle/PFR_file_request.py
@@ -1,10 +1,20 @@
 import jaydebeapi
 import json
 from generating_report_files import *
+from writing_to_log_file import *
 
+#***************************************************************
+name_log = 'PFR_file_request'
+test = 0
+mail = 'IVAbdulganiev@yanao.ru,AVShashkov@yanao.ru'
+root_path = 'PFR'
 
-path = "access_report.txt"
-with open(path) as f:
+#***************************************************************
+writing_to_log_file(name_log, f'***************************************************************')
+
+#***************************************************************
+path_access = "access_report.txt"
+with open(path_access) as f:
     access = json.load(f)
     
 driver = 'ojdbc14.jar'
@@ -22,6 +32,7 @@ conn = jaydebeapi.connect(
 
 curs = conn.cursor()
 
+writing_to_log_file(name_log, f'к базе подключился')
 
 #***************************************************************
 def PFR_file_request_file(pccat_id):
@@ -31,6 +42,7 @@ def PFR_file_request_file(pccat_id):
     curs.execute(sql)
     return curs.fetchall()
 
+#***************************************************************
 def PFR_file_request(pccat_id):
     with open('PFR_file_request.sql', 'r', encoding='utf8') as f:
         sql = f.read()
@@ -38,6 +50,7 @@ def PFR_file_request(pccat_id):
     curs.execute(sql)
     return curs.fetchall()
 
+#***************************************************************
 def PFR_file_request_name():
     curs.execute("""select 
         '0302'||To_Char(TRUNC(LAST_DAY(SYSDATE))+1, 'y')||To_Char(TRUNC(LAST_DAY(SYSDATE))+1, 'mm')||'1.000' 
@@ -45,7 +58,6 @@ def PFR_file_request_name():
     return curs.fetchone()
 
 #***************************************************************
-
 cats_id = [731,732,725,723,727,730,724,728,1108,729,726,1116,1109]
 categories = {}
 for cat_id in cats_id:
@@ -53,25 +65,33 @@ for cat_id in cats_id:
     if len(cat_name_id) != 0:
         name_cat, id = cat_name_id[0]
         categories[name_cat] = id
+writing_to_log_file(name_log, f'категории считал')
 
-
-root_path = 'PFR'
+#***************************************************************
 try:
     shutil.rmtree(root_path, ignore_errors=False, onerror=None)
-except:
-    pass    
+    writing_to_log_file(name_log, f'папку {root_path} удалил')
+except Exception as e:
+    writing_to_log_file(name_log, f'при удалении папки {root_path} произошла ошибка {e}')    
 os.mkdir(root_path)
+writing_to_log_file(name_log, f'папку {root_path} создал')
 
-
+#***************************************************************
 file_name = PFR_file_request_name() # имя файла типа ЗАПРОС
-file_zip = f'РСД Запрос {file_name[0]}.zip' # имя архива
-report = {} # словарь для записи кол-ва строк для акта
+writing_to_log_file(name_log, f'получил имя файла - {file_name[0]}')
 
+file_zip = f'РСД Запрос {file_name[0]}.zip' # имя архива
+writing_to_log_file(name_log, f'получил имя архива - {file_zip}')
+
+#***************************************************************
 try:
     os.remove(file_zip)
-except:
-    pass
+    writing_to_log_file(name_log, f'если есть архив {file_zip}, то его удалил')
+except Exception as e:
+    writing_to_log_file(name_log, f'архива {file_zip} нет, удалять нечего - {e}')
 
+#***************************************************************
+report = {} # словарь для записи кол-ва строк для акта
 for key, value in categories.items():
     data = PFR_file_request(value)
     log = key
@@ -79,8 +99,9 @@ for key, value in categories.items():
     
     try:
         os.mkdir(key) # если каталога нет, то создаем
-    except:
-        pass
+        writing_to_log_file(name_log, f'создал папку {key}')
+    except Exception as e:
+        writing_to_log_file(name_log, f'ошибка при создании папки {key} - {e}')
     
     path = f'{key}/{file_name[0]}' # путь к файлу
     
@@ -96,16 +117,15 @@ for key, value in categories.items():
     
     with zipfile.ZipFile(file_zip, 'a') as myzip:
         myzip.write(path)
+        writing_to_log_file(name_log, f'добавляем {path} в {file_zip}, количество записей - {cnt}')
 
     report[log] = cnt
 
+#***************************************************************
 text_report = ''
 for key, value in report.items():
     text_report += f'{key} - {value} записей\n'
 
-name_log = 'PFR_file_request'
-name_def = file_zip
-test = 0
-mail = 'IVAbdulganiev@yanao.ru,AVShashkov@yanao.ru'
-    
-generating_report_files_PFR_2(name_log, name_def, test, mail, text_report)
+writing_to_log_file(name_log, f'адрес для отправки - {mail}, file_zip - {file_zip}, test - {test}, \n text_report \n {text_report}')
+writing_to_log_file(name_log, f'вызов функции "generating_report_files_PFR_2()"')
+generating_report_files_PFR_2(name_log, file_zip, test, mail, text_report)
