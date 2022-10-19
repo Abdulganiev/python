99c249c032bdca87ddd16ffc99e86be62b5b4a43
Abdulganiev <300195@mail.ru>
2022-10-19 21:42:10 +0500

    addresses added

--- oracle/not_correct_GU_PGS.sql
+++ oracle/not_correct_GU_PGS.sql
@@ -14,6 +14,10 @@ select t.pc_id, t.region_id, t.id, t.message_guid, t.adr, t.request_pdoc_id, t.s
        when upper(t.adr) like '%ЯМАЛЬ%' then 68
        when upper(t.adr) like '%ШУРЫШ%' then 69
        when upper(t.adr) like '%ТАЗ%' then 70
+       when upper(t.adr) like upper('%Башкортостан%') then 60
+       when upper(t.adr) like upper('%Татарстан%') then 60
+       when upper(t.adr) like upper('%Дагестан%') then 60
+       
    else 0 end) as mo_out
 from
 (select
@@ -23,29 +27,64 @@ from
   uszn.pkXMLUtils.GUIDToStr(r.message_guid) as message_guid,
 
   (case
-    when uszn.pkPerson.GetPersonalReq(r.region_id, r.pc_id, 15) is not null
+    when uszn.pkPerson.GetPersonalReq(r.region_id, r.pc_id, 15) is not null -- адрес чела
       then uszn.pkPerson.GetPersonalReq(r.region_id, r.pc_id, 15)
 
-    when (select uszn.StrCommaConcat(value) from uszn.all_personal_doc_reqs q
+    when (select uszn.StrCommaConcat(value) from uszn.all_personal_doc_reqs q -- адрес в паспорте произвольных
            where q.region_id=r.region_id and q.pdoc_id=uszn.pkPerson.GetMainPersonIdentity(r.region_id, r.pc_id, 0) and class_id=8434) is not null
       then (select uszn.StrCommaConcat(value) from uszn.all_personal_doc_reqs q
              where q.region_id=r.region_id and q.pdoc_id=uszn.pkPerson.GetMainPersonIdentity(r.region_id, r.pc_id, 0) and class_id=8434)
 
-    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес из 103-ЗАО
            where q.class_id=14531 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
       then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
              where q.class_id=14531 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
 
-    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес регистрации из ГСП соц.контракт член семьи
            where q.class_id=18784 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
       then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
              where q.class_id=18784 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
 
-    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.контракт член семьи
            where q.class_id=18854 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
       then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
              where q.class_id=18854 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
+
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.помощь член семьи
+           where q.class_id=10336 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
+      then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+             where q.class_id=10336 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
+
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.помощь член семьи
+           where q.class_id=18881 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
+      then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+             where q.class_id=18881 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
              
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.помощь член семьи
+           where q.class_id=18981 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
+      then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+             where q.class_id=18981 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
+             
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.помощь член семьи
+           where q.class_id=18982 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
+      then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+             where q.class_id=18982 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
+             
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.помощь член семьи
+           where q.class_id=10342 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
+      then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+             where q.class_id=10342 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
+             
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.помощь член семьи
+           where q.class_id=18652 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
+      then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+             where q.class_id=18652 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
+
+    when (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q -- адрес проживания из ГСП соц.помощь член семьи
+           where q.class_id=18670 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id) is not null
+      then (select uszn.StrCommaConcat(uszn.pkGen.StripRgnPrefix(decoded_value)) from uszn.all_personal_doc_reqs q
+             where q.class_id=18670 and q.region_id=r.region_id and q.pdoc_id=r.request_pdoc_id)
+
     end) as adr,
 
  r.request_pdoc_id,
