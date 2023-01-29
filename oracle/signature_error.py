9ebf21f5317484876c4cad6284fbe2369fe8f950
Abdulganiev <300195@mail.ru>
2023-01-29 21:57:04 +0500

    add avrora

--- oracle/signature_error.py
+++ oracle/signature_error.py
@@ -33,14 +33,51 @@ def alarm_ep():
         )
     return cnt
 
+def alarm_aurora():
+    curs.execute(
+    '''select count(*) 
+       from uszn.all_smev2_inc_messages
+       where error_message like 'ORA-29516: Aurora%' and date_created>=trunc(sysdate-30, 'dd')''')
+    cnt = int(curs.fetchall()[0][0])
+
+    if cnt > 0:
+        curs.execute(
+        '''
+        declare
+          iiIDs        uszn.pkGen.TIntegers;
+          i$ Pls_Integer;
+        begin
+            select id as id bulk collect into iiIDs
+              from  uszn.all_smev2_inc_messages
+              where error_message like 'ORA-29516: Aurora%' and date_created>=trunc(sysdate-30, 'dd');
+          for i in 1..iiIDs.count loop
+            begin
+              i$ := uszn.pkSMEVProv.AsyncProcessMessage(iiIDs(i),1,1,'Принудительная обработка входящего сообщения СМЭВ-2');
+              commit;
+            exception
+              when Others then
+                rollback;
+                raise;
+            end;
+          end loop;
+        end;
+        '''
+        )
+    return cnt
+
+
+
+
+
 # ********************************************
 
 curs = connect_oracle()
 
-cnt = alarm_ep()
+cnt_ep = alarm_ep()
+cnt_aurora = alarm_aurora()
 
 log = 'signature_error'
-text = f'{cnt}'
+text = f'ep - {cnt_ep}, aurora - {cnt_aurora}'
 
 writing_to_log_file(log, text)
 send_email('IVAbdulganiev@yanao.ru', 'Проверка ЭП сделана', msg_text=text)
