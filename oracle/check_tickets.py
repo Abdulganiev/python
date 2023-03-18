8a2226cfd0a83705c3a84b880c50dfc22aa46bc3
Abdulganiev <300195@mail.ru>
2023-03-18 14:03:25 +0500

    add check_tickets

---
+++ oracle/check_tickets.py
@@ -0,0 +1,60 @@
+from generating_report_files import *
+import pandas as pd
+
+#***************************************************************
+name_log = 'check_tickets'
+test = 0
+mail = 'IVAbdulganiev@yanao.ru'
+name_def = 'Нет права на авиабилет'
+
+#***************************************************************
+def check_tickets(curs):
+    with open('check_tickets.sql', 'r', encoding='utf8') as f:
+        sql = f.read()
+    curs.execute(sql)
+    
+    data = {
+         'id района' : [],
+         'name' : [] ,
+         'Наименование района' : [],
+         'id человека' : [] ,
+         'ФИО др' : [] ,
+         'Возраст' : [] ,
+         'Дата окончания права на покупку билета' : [] ,
+         'Дата окончания - Ребёнок до 18 лет (учащ. до 23 лет) в составе семьи «Дети-родители» или «Опекаемые-опекуны» с 3 и более детьми до 18 лет, учащ. до 23 лет' : [] ,
+            }
+    for row in curs.fetchall():
+        data['id района'].append(row[0])
+        data['name'].append(row[1])
+        data['Наименование района'].append(row[2])
+        data['id человека'].append(row[3])
+        data['ФИО др'].append(row[4])
+        data['Возраст'].append(row[5])
+        data['Дата окончания права на покупку билета'].append(row[6])
+        data['Дата окончания - Ребёнок до 18 лет (учащ. до 23 лет) в составе семьи «Дети-родители» или «Опекаемые-опекуны» с 3 и более детьми до 18 лет, учащ. до 23 лет'].append(row[7])
+
+    return data
+
+#***************************************************************
+
+writing_to_log_file(name_log, f'***************************************************************')
+try:
+    curs = connect_oracle()
+except Exception as e:    
+    text = f'произошла ошибка при вызове функции connect_oracle() - {e}'
+    alarm_log(mail, name_log, text)
+
+try:
+    data = pd.DataFrame(check_tickets(curs))
+except Exception as e:
+    text = f'произошла ошибка при вызове функции check_tickets() - {e}'
+    alarm_log(mail, name_log, text)
+
+writing_to_log_file(name_log, f'Количество записей - {len(data)}')
+
+for mo in sorted(set(data['name'])):
+    cnt = data[data['name'].isin([mo])].count()[0]
+    text = f'{mo} - {cnt} строк'
+    writing_to_log_file(name_log, text)
+    
+generating_report_files(data, name_log, name_def, test, mail)
